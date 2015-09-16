0cadc6a2dfd40b550f2d27fbae5a63814addd6ae
vev1976 <vev1976@gmail.com>
2015-09-16 13:15:20 +0300

    3

--- AbisGen/src/com/abisgen/main/db2interface.java
+++ AbisGen/src/com/abisgen/main/db2interface.java
@@ -6,64 +6,64 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-//"jdbc:db2://10.130.33.3:50008/testvev", "techuser", "123start")
+
 public class db2interface {
-	private String driver = "jdbc:db2://";
-	private String server = "/";
-	private String database = "";
-	private String UID = "";
-	private String PWD = "";
-	
-	private Connection con;
-	
-	public db2interface() throws SQLException
-	{
-		con = DriverManager.getConnection(driver+server+database, UID, PWD);
-	}
-	
-	public void ExecSQL(String sql) throws SQLException
-	{
-		Statement stm = con.createStatement();		
-		stm.executeUpdate(sql);
-		stm.close();
-		con.commit();
-	}
-	
-	public String[] SelectOneRow(String sql)
-	{
-		Statement stm = null;
-		String[] s = {};
-		
-		
-		try {
-			stm = con.createStatement();
-			ResultSet rs = stm.executeQuery(sql);
-			if (rs.next())
-			{
-				 ResultSetMetaData rsmd = rs.getMetaData();	
-				 s = new String[rsmd.getColumnCount()];
-				 
-				 for(int i=0;i<rsmd.getColumnCount();i++)	
-				 {
-					 s[i] = rs.getString(i+1);
-				 }	 
-			 }			
-		} catch (SQLException e) {		
-			e.printStackTrace();
-		}
-		finally
-		{
-			try {
-				if (stm!=null)
-				    stm.close();
-				con.commit();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-		}
-		return s;
-	}
+    private String driver = "jdbc:db2://";
+    private String server = "/";
+    private String database = "";
+    private String UID = "";
+    private String PWD = "";
+    
+    private Connection con;
+    
+    public db2interface() throws SQLException
+    {
+        con = DriverManager.getConnection(driver+server+database, UID, PWD);
+    }
+    
+    public void ExecSQL(String sql) throws SQLException
+    {
+        Statement stm = con.createStatement();
+        stm.executeUpdate(sql);
+        stm.close();
+        con.commit();
+    }
+    
+    public String[] SelectOneRow(String sql)
+    {
+        Statement stm = null;
+        String[] s = {};
+        
+        
+        try {
+            stm = con.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            if (rs.next())
+            {
+                 ResultSetMetaData rsmd = rs.getMetaData();
+                 s = new String[rsmd.getColumnCount()];
+                 
+                 for(int i=0;i<rsmd.getColumnCount();i++)
+                 {
+                     s[i] = rs.getString(i+1);
+                 }
+             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try {
+                if (stm!=null)
+                    stm.close();
+                con.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            
+        }
+        return s;
+    }
 
 }
 
