d3de76226b5657ac827f2fb352a845626652bd5d
vev1976 <vev1976@gmail.com>
2015-09-16 12:23:05 +0300

    second

--- AbisGen/src/com/abisgen/main/MainWindow.java
+++ AbisGen/src/com/abisgen/main/MainWindow.java
@@ -1,6 +1,8 @@
 package com.abisgen.main;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,6 +15,7 @@ public class MainWindow extends Frame
     private int win_hight = 600;
     private WinAdapter winadapter = new WinAdapter();
     private Frame self;
+    private TextArea ta_log;
     
     public InputParams inputparams;
     
@@ -33,10 +36,59 @@ public class MainWindow extends Frame
          this.setLocation((screen_dim.width-win_width)/2, (screen_dim.height-win_hight)/2);
          this.addWindowListener(winadapter);
          this.BuildFace();
+         
     }
     
     private void BuildFace()
     {
+         this.setLayout(new GridBagLayout());
+         GridBagConstraints c = new GridBagConstraints();
+         
+         c.weightx = 0.8;
+         c.weighty = 0.3;
+         
+         Panel p_input = new Panel();
+         c.fill = GridBagConstraints.HORIZONTAL;
+         c.gridx = 0;
+         c.gridy = 0;
+         c.anchor = GridBagConstraints.NORTHWEST;
+         this.add(p_input,c);
+         
+         Panel p_buttons = new Panel(new GridLayout(1,3));
+         c.weightx = 0.2;
+         c.weighty = 0.3;
+         c.fill = GridBagConstraints.NONE;
+         c.gridx = 1;
+         c.gridy = 0;
+         c.anchor = GridBagConstraints.NORTHEAST;
+         this.add(p_buttons,c);
+         
+         Button btnExit = new Button();
+         btnExit.setLabel("Exit");
+    //     btnCancel.setPreferredSize(new Dimension(70,22));
+         btnExit.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e)
+             {
+                 System.exit(0);
+             }
+          });
+         p_buttons.add(btnExit);
+         
+         Panel p_log = new Panel(new GridLayout(1,1));
+         c.weightx = 1;
+         c.weighty = 0.7;
+         c.fill = GridBagConstraints.BOTH;
+         c.gridx = 0;
+         c.gridy = 1;
+         c.gridwidth = 2;
+         c.anchor = GridBagConstraints.CENTER;
+         this.add(p_log,c);
+         
+         ta_log = new TextArea();
+         
+         p_log.add(ta_log);
         
+         
+         
     }
 }
